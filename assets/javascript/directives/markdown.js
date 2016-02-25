61df089f6151a18994fff74cf8740fbd62a88907
Daniel Martinez <dmartz86@gmail.com>
2016-02-25 18:38:15 +0000

    Add toggle action to see content

--- assets/javascript/directives/markdown.js
+++ assets/javascript/directives/markdown.js
@@ -15,16 +15,20 @@
   }
 
   var template = 
-'<div class=" post col-xs-12 col-sm-12 col-md-12 col-lg-12">' + 
-  '<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">' +
-    '<img ng-src="{{item.image}}" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">' +
-  '</div>' +
-  '<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">' +
+'<div class=" post col-xs-12 col-sm-12 col-md-12 col-lg-6">' + 
+  '<div">' +
     '<h2 class="post-title">{{item.title}}</h2>' +
     '<h3 class="post-subtitle">{{item.subtitle}}</h3>' +
     '<p class="post-info">Posted by <a href="{{item.profile}}">{{item.author}}</a> on {{item.date}}</p>' +
-    '<div class="post-content"></div>' +
-    '<a href="/posts/{{item._id}}">See full post</a>' +
+    '<div ng-show="item.more" class="post-summary"></div>' +
+    '<div ng-show="item.more" class="post-content"></div>' +
+    '<a ng-show="!item.more" ng-click="item.toggle()" href="" class="btn btn-primary col-md-3">' +
+      '<i class="fa fa-angle-down fa-3x"></i></a>' +
+    '<a ng-show="item.more" ng-click="item.toggle()" "href="" class="btn btn-primary col-md-3">' +
+      '<i class="fa fa-angle-up fa-3x"></i></a>' +
+    '<a href="" class="btn btn-primary col-md-3"><i class="fa fa-share-alt fa-3x"></i></a>' +
+    '<a href="/posts/{{item._id}}" class="btn btn-success col-md-3"><i class="fa fa-edit fa-3x"></i></a>' +
+    '<a ng-click="item.disable()" href="" class="btn btn-danger col-md-3"><i class="fa fa-trash fa-3x"></i></a>' +
   '</div>' +
 '</div>';
 
@@ -37,12 +41,34 @@
 
     function link (scope, element) {
 
-      function does (ele) {
-        var container = ele.find('.post-content')[0];
-        ReactDOM.render(React.createElement(Markdown.editor, {post: scope.item.summary || ''}), container);
+      scope.item.more = false;
+      scope.item.toggle = toggle;
+
+      function toggle () {
+        scope.item.more = !scope.item.more;
       }
 
-      does(element);
+      function disable () {
+        scope.item.disabled = true;
+        //TODO: Ajax event
+        //TODO: Reload list
+      }
+
+      function render (ele) {
+        ReactDOM.render(
+          React.createElement(Markdown.editor, 
+          {post: scope.item.summary || ''}),
+          ele.find('.post-summary')[0]
+        );
+
+        ReactDOM.render(
+          React.createElement(Markdown.editor,
+          {post: scope.item.content || ''}),
+          ele.find('.post-content')[0]
+        );
+      }
+
+      render(element);
     }
   } 
 
